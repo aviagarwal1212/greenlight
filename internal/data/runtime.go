de046cb91d4c8ae5d589fc2f333604e98619dd7b
Avi Agarwal <aviagarwal1212@gmail.com>
2024-06-12 20:38:43 +0530

    completed ch4.4
    - added a json unmarshaler interface for Runtime to accept "<runtime>
    mins" format in json body

--- internal/data/runtime.go
+++ internal/data/runtime.go
@@ -1,14 +1,44 @@
 package data
 
 import (
+	"errors"
 	"fmt"
 	"strconv"
+	"strings"
 )
+
+var ErrInvalidRuntimeFormat = errors.New("invalid runtime format")
 
 type Runtime int32
 
+// implements the MarshalJSON() method on Runtime so that
+// it satisfies the json.Marshaler interface
 func (r Runtime) MarshalJSON() ([]byte, error) {
 	jsonValue := fmt.Sprintf("%d mins", r)
 	quotedValue := strconv.Quote(jsonValue)
 	return []byte(quotedValue), nil
+}
+
+// implements the UnmarshalJSON() method on Runtime so that
+// it satisfies the json.Unmarshler interface
+// Note: uses *Runtime instead of Runtime because it modifies the receiver
+func (r *Runtime) UnmarshalJSON(jsonValue []byte) error {
+	// the incoming JSON value will be a string of the format "<runtime> mins"
+	unquotesJSONValue, err := strconv.Unquote(string(jsonValue))
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	parts := strings.Split(unquotesJSONValue, " ")
+	if len(parts) != 2 || parts[1] != "mins" {
+		return ErrInvalidRuntimeFormat
+	}
+
+	value, err := strconv.Atoi(parts[0])
+	if err != nil {
+		return ErrInvalidRuntimeFormat
+	}
+
+	*r = Runtime(value)
+	return nil
 }
